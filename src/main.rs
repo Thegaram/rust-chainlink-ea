79bbc455cee492e71dab7f4bd7d337ad964f937b
Thegaram <th307q@gmail.com>
2020-12-23 18:02:30 +0800

    add job parsing and update listen address

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,32 @@
 // RUST_LOG='rust-cl-ea=info' ./target/release/rust-chainlink-ea
 
 use log::info;
+use serde::{Serialize, Deserialize};
 use std::convert::Infallible;
 use warp::Filter;
 
-const ADDRESS: [u8; 4] = [127, 0, 0, 1];
+const ADDRESS: [u8; 4] = [0, 0, 0, 0];
 const PORT: u16 = 1234;
 
-async fn process_job(job: serde_json::Value) -> Result<impl warp::Reply, Infallible> {
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+struct Job {
+    id: String,
+    data: JobData,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+struct JobData {
+    function_selector: String,
+    result: String,
+}
+
+fn parse_job() -> impl Filter<Extract = (Job,), Error = warp::Rejection> + Clone {
+    warp::body::content_length_limit(1024 * 16).and(warp::body::json())
+}
+
+async fn process_job(job: Job) -> Result<impl warp::Reply, Infallible> {
     info!(target: "rust-cl-ea", "new job request: {:?}", job);
     Ok(warp::http::StatusCode::OK)
 }
@@ -24,7 +43,7 @@ async fn main() {
     // POST /
     let job = warp::path::end()
         .and(warp::post())
-        .and(warp::body::json())
+        .and(parse_job())
         .and_then(process_job);
 
     let routes = health.or(job).with(warp::log("rust-cl-ea"));
